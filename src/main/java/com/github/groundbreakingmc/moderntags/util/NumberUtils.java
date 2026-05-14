d715fc3d255e27f4268fae5958e6aa76fbc9ca61
GroundbreakingMC <noslowdwn@gmail.com>
2026-05-15 01:20:31 +0200

    fix(utils): fix incorrect health display formatting
    pum.. purum...

---
+++ src/main/java/com/github/groundbreakingmc/moderntags/util/NumberUtils.java
@@ -0,0 +1,32 @@
+package com.github.groundbreakingmc.moderntags.utils;
+
+public class NumberUtils {
+
+    public static String doubleToStr(double v) {
+        final int normalized = (int) Math.ceil(v);
+        return switch (normalized) {
+            case 0 -> "0";
+            case 1 -> "1";
+            case 2 -> "2";
+            case 3 -> "3";
+            case 4 -> "4";
+            case 5 -> "5";
+            case 6 -> "6";
+            case 7 -> "7";
+            case 8 -> "8";
+            case 9 -> "9";
+            case 10 -> "10";
+            case 11 -> "11";
+            case 12 -> "12";
+            case 13 -> "13";
+            case 14 -> "14";
+            case 15 -> "15";
+            case 16 -> "16";
+            case 17 -> "17";
+            case 18 -> "18";
+            case 19 -> "19";
+            case 20 -> "20";
+            default -> Integer.toString(normalized);
+        };
+    }
+}
